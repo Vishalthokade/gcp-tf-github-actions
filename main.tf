90dc00dfc147ebd3358288d855437ff2f0422057
Arun <anthokade@gmail.com>
2024-05-05 11:34:32 +0530

    made some changes

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 resource "google_storage_bucket" "my-bucket-01" {
   name                     = "tt-github-action-demo-bucket-01"
-  id                       = "central-rampart-421909-r3"
+  project                  = "central-rampart-421909-r3"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+
+
